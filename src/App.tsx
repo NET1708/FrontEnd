16cabf71dcc5e2d30744795159fff8689d253f66
Phạm Quang Vinh <Vinhpqse160633@fpt.edu.vn>
2024-03-02 23:37:56 +0700

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './layouts/header-footer/Navbar';
+import Footer from './layouts/header-footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
 }
