fac6028f0ac92bb50ddd0910c605776831d48d3e
Phạm Quang Vinh <Vinhpqse160633@fpt.edu.vn>
2024-03-21 11:26:02 +0700

    Update generateQRCode.tsx

--- src/layouts/utils/generateQRCode.tsx
+++ src/layouts/utils/generateQRCode.tsx
@@ -1,10 +1,13 @@
-import axios from 'axios';
-
 const generateQRCode = async (accountNo: string, accountName: string, acqId: number, amount: number, addInfo: string) => {
   try {
-    const response = await axios.post(
-      'https://api.vietqr.io/v2/generate',
-      {
+    const response = await fetch('https://api.vietqr.io/v2/generate', {
+      method: 'POST',
+      headers: {
+        'x-client-id': '838965fd-4de5-4c7f-9ff7-c0fef3ef4936',
+        'x-api-key': 'ea7d3017-a0d0-4a13-a152-bc3c85e42c2e',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
         accountNo,
         accountName,
         acqId,
@@ -12,21 +15,13 @@ const generateQRCode = async (accountNo: string, accountName: string, acqId: num
         addInfo,
         format: 'text',
         template: 'compact2',
-      },
-      {
-        headers: {
-          'x-client-id': '838965fd-4de5-4c7f-9ff7-c0fef3ef4936',
-          'x-api-key': 'ea7d3017-a0d0-4a13-a152-bc3c85e42c2e',
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-
-    return response.data.data.qrDataURL;
+      }),
+    });
+    const responseData = await response.json();
+    return responseData.data.qrDataURL;
   } catch (error) {
-    console.error('Error generating QR code:', error);
-    return null;
+    console.error(error);
   }
-};
+}
 
 export default generateQRCode;
